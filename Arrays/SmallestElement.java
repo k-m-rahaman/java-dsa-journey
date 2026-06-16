8782ee330597f0c9d9c5c26220c062c8156e2e4b
k-m-rahaman <kmrofficial786@gmail.com>
2026-06-17 05:10:12 +0530

    Added smallest element program

---
+++ Arrays/SmallestElement.java
@@ -0,0 +1,18 @@
+public class SmallestElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 7, 99, 43};
+
+        int smallest = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+
+            if(arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Smallest Element: " + smallest);
+    }
+}
