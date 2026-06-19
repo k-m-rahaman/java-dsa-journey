2750e318b7d896a511a767af6348a2e7d2145397
k-m-rahaman <kmrofficial786@gmail.com>
2026-06-20 05:18:39 +0530

    Added remove duplicates program

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,16 @@
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,2,2,3,4,4};
+
+        System.out.print("Unique Elements: ");
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(i == 0 || arr[i] != arr[i - 1]) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
