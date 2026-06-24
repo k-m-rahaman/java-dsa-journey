78f3440189349df9c91dbd802f40176de1de7f1e
k-m-rahaman <kmrofficial786@gmail.com>
2026-06-25 05:08:46 +0530

    Added string length program

---
+++ Strings/StringLength.java
@@ -0,0 +1,10 @@
+public class StringLength {
+
+    public static void main(String[] args) {
+
+        String str = "Programming";
+
+        System.out.println(
+                "Length = " + str.length());
+    }
+}
