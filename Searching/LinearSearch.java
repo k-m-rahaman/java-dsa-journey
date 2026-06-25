733eaf29b8872224e2225c4511c8feb95285135e
k-m-rahaman <kmrofficial786@gmail.com>
2026-06-26 05:09:27 +0530

    Added linear search program

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,17 @@
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 12, 8, 20, 15};
+        int target = 20;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == target) {
+                System.out.println("Found at index " + i);
+                return;
+            }
+        }
+
+        System.out.println("Element not found");
+    }
+}
