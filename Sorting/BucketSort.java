acf6f2db602cec73244f89d08ad2274805830c89
k-m-rahaman <kmrofficial786@gmail.com>
2026-07-04 05:34:13 +0530

    Implemented Bucket Sort

--- Sorting/BucketSort.java
+++ Sorting/BucketSort.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class BucketSort {
 
@@ -7,27 +8,25 @@ public class BucketSort {
 
         int n = arr.length;
 
-        ArrayList<Float>[] buckets = new ArrayList[n];
+        List<List<Float>> buckets = new ArrayList<>();
 
-        for (int i = 0; i < n; i++)
-            buckets[i] = new ArrayList<>();
-
-        for (float num : arr) {
-
-            int index = (int) (num * n);
-
-            buckets[index].add(num);
+        for (int i = 0; i < n; i++) {
+            buckets.add(new ArrayList<>());
         }
 
-        for (ArrayList<Float> bucket : buckets)
+        for (float num : arr) {
+            int index = (int) (num * n);
+            buckets.get(index).add(num);
+        }
+
+        for (List<Float> bucket : buckets) {
             Collections.sort(bucket);
+        }
 
         int index = 0;
 
-        for (ArrayList<Float> bucket : buckets) {
-
+        for (List<Float> bucket : buckets) {
             for (float num : bucket) {
-
                 arr[index++] = num;
             }
         }
@@ -35,19 +34,12 @@ public class BucketSort {
 
     public static void main(String[] args) {
 
-        float[] arr = {
-                0.42f,
-                0.32f,
-                0.23f,
-                0.52f,
-                0.25f,
-                0.47f,
-                0.51f
-        };
+        float[] arr = {0.42f, 0.32f, 0.23f, 0.52f, 0.25f, 0.47f, 0.51f};
 
         bucketSort(arr);
 
-        for (float num : arr)
+        for (float num : arr) {
             System.out.print(num + " ");
+        }
     }
 }
