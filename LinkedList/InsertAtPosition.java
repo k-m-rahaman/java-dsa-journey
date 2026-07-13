10e4277bef5d46a7eb697abdb192eaf3faa7943b
k-m-rahaman <kmrofficial786@gmail.com>
2026-07-13 06:02:08 +0530

    Implemented insertion at specific position

---
+++ LinkedList/InsertAtPosition.java
@@ -0,0 +1,65 @@
+public class InsertAtPosition {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void insert(int position, int data) {
+
+        Node newNode = new Node(data);
+
+        if (position == 1) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        for (int i = 1; i < position - 1 && temp != null; i++) {
+            temp = temp.next;
+        }
+
+        if (temp == null) {
+            System.out.println("Invalid Position");
+            return;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void display() {
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        InsertAtPosition list = new InsertAtPosition();
+
+        list.insert(1, 10);
+        list.insert(2, 20);
+        list.insert(3, 40);
+
+        list.insert(3, 30);
+
+        list.display();
+    }
+}
