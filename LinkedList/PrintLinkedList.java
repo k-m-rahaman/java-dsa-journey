4accc27191b4d790520aca04da0cdaa6890f2e7f
k-m-rahaman <kmrofficial786@gmail.com>
2026-07-17 06:09:10 +0530

    Implemented linked list traversal

---
+++ LinkedList/PrintLinkedList.java
@@ -0,0 +1,62 @@
+public class PrintLinkedList {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void insert(int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    public void printList() {
+
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        PrintLinkedList list = new PrintLinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+
+        list.printList();
+    }
+}
