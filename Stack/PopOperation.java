3563291a7eb60781e3f0ba32b161e483402268ed
k-m-rahaman <kmrofficial786@gmail.com>
2026-07-20 05:49:42 +0530

    Implemented pop operation

---
+++ Stack/PopOperation.java
@@ -0,0 +1,53 @@
+public class PopOperation {
+
+    int maxSize = 5;
+    int[] stack = new int[maxSize];
+    int top = -1;
+
+    public void push(int data) {
+
+        if (top == maxSize - 1) {
+            System.out.println("Stack Overflow");
+            return;
+        }
+
+        stack[++top] = data;
+    }
+
+    public int pop() {
+
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+
+        return stack[top--];
+    }
+
+    public void display() {
+
+        if (top == -1) {
+            System.out.println("Stack is Empty");
+            return;
+        }
+
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stack[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PopOperation stack = new PopOperation();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Popped Element = " + stack.pop());
+
+        System.out.println();
+
+        stack.display();
+    }
+}
