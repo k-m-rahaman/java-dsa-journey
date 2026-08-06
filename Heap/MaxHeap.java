de0b81afa80d1b1ec3cb09bfc29a9d1c170d3504
k-m-rahaman <kmrofficial786@gmail.com>
2026-08-06 07:17:40 +0530

    Implemented max heap using priority queue

---
+++ Heap/MaxHeap.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MaxHeap {
+
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> maxHeap =
+                new PriorityQueue<>(Collections.reverseOrder());
+
+        maxHeap.offer(40);
+        maxHeap.offer(10);
+        maxHeap.offer(30);
+        maxHeap.offer(20);
+        maxHeap.offer(50);
+
+        System.out.println("Max Heap:");
+
+        while (!maxHeap.isEmpty()) {
+            System.out.print(maxHeap.poll() + " ");
+        }
+    }
+}
