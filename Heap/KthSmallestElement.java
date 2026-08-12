832cc5c72111287533cee632842df242ce9260b1
k-m-rahaman <kmrofficial786@gmail.com>
2026-08-12 07:25:21 +0530

    Implemented kth smallest element using heap

---
+++ Heap/KthSmallestElement.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthSmallestElement {
+
+    public static int findKthSmallest(int[] nums, int k) {
+
+        PriorityQueue<Integer> maxHeap =
+                new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int num : nums) {
+
+            maxHeap.offer(num);
+
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        return maxHeap.peek();
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {7, 10, 4, 3, 20, 15};
+
+        int k = 3;
+
+        System.out.println("Kth Smallest Element = " + findKthSmallest(nums, k));
+    }
+}
