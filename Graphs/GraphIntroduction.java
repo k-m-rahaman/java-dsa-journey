a1008367cf9795c899fe69d28135f1e113556ed5
k-m-rahaman <kmrofficial786@gmail.com>
2026-08-14 07:36:13 +0530

    Implemented graph introduction

---
+++ Graphs/GraphIntroduction.java
@@ -0,0 +1,17 @@
+public class GraphIntroduction {
+
+    public static void main(String[] args) {
+
+        int vertices = 5;
+        int edges = 6;
+
+        System.out.println("Graph Introduction");
+        System.out.println("-------------------");
+        System.out.println("Number of Vertices: " + vertices);
+        System.out.println("Number of Edges: " + edges);
+
+        System.out.println("\nA Graph consists of:");
+        System.out.println("1. Vertices (Nodes)");
+        System.out.println("2. Edges (Connections)");
+    }
+}
