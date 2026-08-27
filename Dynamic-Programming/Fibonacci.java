fc0e83c3a7c1be740e9fde8f168b88ca286648b0
k-m-rahaman <kmrofficial786@gmail.com>
2026-08-27 07:17:10 +0530

    Implemented recursive Fibonacci

---
+++ Dynamic-Programming/Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+
+    static int fibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        System.out.println("Fibonacci Number = " + fibonacci(n));
+    }
+}
