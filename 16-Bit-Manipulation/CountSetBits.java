309c8c635bb2e9dd84da072d4c5346e11d1be5e2
k-m-rahaman <kmrofficial786@gmail.com>
2026-09-24 07:29:54 +0530

    Implemented set bit counting using bit manipulation

---
+++ 16-Bit-Manipulation/CountSetBits.java
@@ -0,0 +1,26 @@
+public class CountSetBits {
+
+    static int countSetBits(int number) {
+
+        int count = 0;
+
+        while (number != 0) {
+
+            number = number & (number - 1);
+
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int number = 13;
+
+        System.out.println(
+                "Number of set bits: "
+                        + countSetBits(number)
+        );
+    }
+}
