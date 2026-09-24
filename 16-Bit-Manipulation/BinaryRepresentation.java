33cb63eadbcacb66e349ff4d3e6a49969c48d69a
k-m-rahaman <kmrofficial786@gmail.com>
2026-09-24 07:28:19 +0530

    Implemented binary representation using bit manipulation

---
+++ 16-Bit-Manipulation/BinaryRepresentation.java
@@ -0,0 +1,32 @@
+public class BinaryRepresentation {
+
+    static String toBinary(int number) {
+
+        if (number == 0) {
+            return "0";
+        }
+
+        StringBuilder binary = new StringBuilder();
+
+        while (number > 0) {
+
+            int bit = number & 1;
+
+            binary.append(bit);
+
+            number = number >> 1;
+        }
+
+        return binary.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+        int number = 25;
+
+        System.out.println(
+                "Binary of " + number + ": "
+                        + toBinary(number)
+        );
+    }
+}
