ab61a2254e832b3b095c6cfe87bef3327dda7052
k-m-rahaman <kmrofficial786@gmail.com>
2026-09-26 07:41:05 +0530

    Implemented missing number using XOR

---
+++ 16-Bit-Manipulation/SingleNumber.java
@@ -0,0 +1,29 @@
+// If every number appears twice except one:
+
+// x ^ x = 0
+// x ^ 0 = x
+
+// So XOR all numbers.
+
+public class SingleNumber {
+
+    static int findSingle(int[] nums) {
+        int result = 0;
+
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {4, 1, 2, 1, 2};
+
+        System.out.println("Single number: " + findSingle(nums));
+    }
+}
+
+// Missing number: 2
+
